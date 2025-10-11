d15878d570ef8f499eebf6f1160ba62f4e0e1576
Tapeline <mail@tapeline.dev>
2025-10-11 23:36:22 +0500

    Fix deployment and account service on the go :)

--- AccountService/api/exceptions.py
+++ AccountService/api/exceptions.py
@@ -1,45 +1,52 @@
+from typing import Any
+
+from django.http.response import Http404
+
 from rest_framework import status
 from rest_framework.exceptions import (
     APIException,
     ErrorDetail,
-    PermissionDenied,
+    PermissionDenied, NotFound,
 )
+from rest_framework.views import set_rollback
+from django.core.exceptions import PermissionDenied as DjangoPermissionDenied
 
 
 class RegistrationDisabledException(PermissionDenied):
     """Raised when user tries to register when registration is disallowed."""
 
-    detail = ErrorDetail(
-        "Registration temporarily disabled",
-        code="REGISTRATION_DISABLED",
-    )
+    default_detail = {
+        "detail": "Registration temporarily disabled",
+        "code": "REGISTRATION_DISABLED",
+    }
+    status_code = status.HTTP_403_FORBIDDEN
 
 
 class UserAlreadyExistsException(APIException):
     """Raised when user tries to register when this username already taken."""
 
-    detail = ErrorDetail(
-        "User with such name is already registered",
-        code="ALREADY_REGISTERED",
-    )
+    default_detail = {
+        "detail": "User with such name is already registered",
+        "code": "ALREADY_REGISTERED",
+    }
     status_code = status.HTTP_400_BAD_REQUEST
 
 
 class PasswordTooShortException(APIException):
     """Raised when password is too short."""
 
-    detail = ErrorDetail(
-        "Password too short",
-        code="PASSWORD_TOO_SHORT",
-    )
+    default_detail = {
+        "detail": "Password too short",
+        "code": "PASSWORD_TOO_SHORT"
+    }
     status_code = status.HTTP_400_BAD_REQUEST
 
 
 class PasswordTooCommonException(APIException):
     """Raised when password is too common."""
 
-    detail = ErrorDetail(
-        "Password too common",
-        code="PASSWORD_TOO_COMMON",
-    )
+    default_detail = {
+        "detail": "Password too common",
+        "code": "PASSWORD_TOO_COMMON"
+    }
     status_code = status.HTTP_400_BAD_REQUEST
