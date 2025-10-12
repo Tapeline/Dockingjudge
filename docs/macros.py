5bf1b8692a43ed8867b44b0b730e22da8dcb7522
Tapeline <mail@tapeline.dev>
2025-10-13 01:13:19 +0500

    Snapshot

--- docs/macros.py
+++ docs/macros.py
@@ -1,9 +1,59 @@
 import textwrap
 from pathlib import Path
+from typing import Final
+
+_GLOSSARY: Final = "docs/reference/glossary.md"
+
+
+def _read_glossary(glossary_path: str) -> dict[str, str]:
+    md_text = Path(glossary_path).read_text()
+    lines = md_text.splitlines()
+    terms = {}
+    i = 0
+    current_term = None
+    current_term_lines = []
+    while i < len(lines):
+        if lines[i].startswith("**term:"):
+            if current_term_lines:
+                terms[current_term] = "\n".join(current_term_lines)
+                current_term = None
+                current_term_lines.clear()
+            current_term = lines[i].removeprefix("**term:").removesuffix("**")
+        elif lines[i] == "" or lines[i].startswith(" "):
+            current_term_lines.append(lines[i].strip())
+        elif lines[i].startswith(":"):
+            current_term_lines.append(lines[i][1:].strip())
+        else:
+            if current_term_lines:
+                terms[current_term] = "\n".join(current_term_lines)
+                current_term = None
+                current_term_lines.clear()
+        i += 1
+    if current_term_lines:
+        terms[current_term] = "\n".join(current_term_lines)
+    return terms
 
 
 def define_env(env):
+    terms = _read_glossary(_GLOSSARY)
+
     @env.macro
     def indent_snippet(path, indent):
         contents = Path(path).read_text()
         return textwrap.indent(contents, indent)
+
+    @env.macro
+    def definition(term: str, indent: str = ""):
+        if term not in terms:
+            return textwrap.indent("Definition not found.", indent)
+        return textwrap.indent(terms[term], indent)
+
+    @env.macro
+    def decorated_definition(term: str, indent: str = ""):
+        if term not in terms:
+            return textwrap.indent("> Definition not found.", indent)
+        md = textwrap.indent(
+            f"**{term}**\n\n{terms[term]}",
+            "> "
+        )
+        return textwrap.indent(md, indent)
